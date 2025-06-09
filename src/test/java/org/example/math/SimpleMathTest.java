3c42e26a9e42e34d83b40f63a90aebb9e8f5ec22
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-09 14:06:35 -0300

    outras equals

--- src/test/java/org/example/math/SimpleMathTest.java
+++ src/test/java/org/example/math/SimpleMathTest.java
@@ -2,7 +2,7 @@ package org.example.math;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleMathTest {
     @Test
@@ -10,6 +10,8 @@ public class SimpleMathTest {
         SimpleMath math = new SimpleMath();
         Double actual = math.sum(6.2D, 2D);
         double expected = 8.2D;
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "My message");
+        assertNotEquals(9.2, actual);
+        assertNotNull(actual);
     }
 }
