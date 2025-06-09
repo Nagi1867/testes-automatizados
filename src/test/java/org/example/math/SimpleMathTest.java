9e432f0c9e37844adc57cbca1cc761d4ab4eecab
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-09 14:12:07 -0300

    lazy asset messages

--- src/test/java/org/example/math/SimpleMathTest.java
+++ src/test/java/org/example/math/SimpleMathTest.java
@@ -8,9 +8,11 @@ public class SimpleMathTest {
     @Test
     void TestSum() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
-        assertEquals(expected, actual, "My message");
+        assertEquals(expected, actual, () ->  firstNumber + "+" + secondNumber + " did not produce " + expected);
         assertNotEquals(9.2, actual);
         assertNotNull(actual);
     }
