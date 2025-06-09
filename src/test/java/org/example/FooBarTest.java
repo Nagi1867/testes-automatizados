01ec4a2e84c4ab99f65a1d2861f4ec32bb5b429c
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-09 13:26:10 -0300

    criando o primeiro teste

---
+++ src/test/java/org/example/FooBarTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FooBarTest {
+    @Test
+    void Test() {
+        System.out.println("Hello World!");
+    }
+}
