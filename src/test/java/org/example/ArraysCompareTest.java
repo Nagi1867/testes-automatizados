964cb6cd0ef207feff57b8d70e696b647e521da6
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-09 22:06:42 -0300

    timeout

--- src/test/java/org/example/ArraysCompareTest.java
+++ src/test/java/org/example/ArraysCompareTest.java
@@ -2,8 +2,10 @@ package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class ArraysCompareTest {
     @Test
@@ -14,5 +16,15 @@ public class ArraysCompareTest {
         Arrays.sort(numbers);
 
         assertArrayEquals(numbers, expectedArray);
+    }
+
+    @Test
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int[] numbers = {25, 8, 21, 32, 3};
+        for (int i = 0; i < 1000000000; i++) {
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
     }
 }
