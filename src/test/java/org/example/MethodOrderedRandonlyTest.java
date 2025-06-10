3fd1bda17d066508fc7891112501768de2b55d3c
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-10 14:22:11 -0300

    TestInstance

--- src/test/java/org/example/MethodOrderedRandonlyTest.java
+++ src/test/java/org/example/MethodOrderedRandonlyTest.java
@@ -1,26 +1,34 @@
 package org.example;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
-@Order(1)
+//@Order(1)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MethodOrderedRandOnlyTest {
+    StringBuilder actualValue = new StringBuilder("");
+
+    @AfterEach
+    void AfterEach() {
+        System.out.println("The actual value is: " + actualValue);
+    }
+
     @Test
     @Order(1)
     void testA() {
         System.out.println("A");
+        actualValue.append("1");
     }
     @Test
     @Order(3)
     void testB() {
         System.out.println("B");
+        actualValue.append("2");
     }
     @Test
     @Order(2)
     void testC() {
         System.out.println("C");
+        actualValue.append("3");
     }
 }
