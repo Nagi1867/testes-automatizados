4e30e3292a71ece77a4077ade316916c2f763cfd
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-10 13:41:44 -0300

    TestMethodOrder order

--- src/test/java/org/example/MethodOrderedRandonlyTest.java
+++ src/test/java/org/example/MethodOrderedRandonlyTest.java
@@ -1,20 +1,24 @@
 package org.example;
 
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class MethodOrderedRandOnlyTest {
     @Test
+    @Order(1)
     void testA() {
         System.out.println("A");
     }
     @Test
+    @Order(3)
     void testB() {
         System.out.println("B");
     }
     @Test
+    @Order(2)
     void testC() {
         System.out.println("C");
     }
