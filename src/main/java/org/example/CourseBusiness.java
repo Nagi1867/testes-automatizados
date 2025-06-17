01ba17e39d88c66473789d8aee26605db00ce8d2
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-16 21:02:40 -0300

    adicionando as novas classes

---
+++ src/main/java/org/example/CourseBusiness.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.service.CourseService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseBusiness {
+    private CourseService service;
+
+    public CourseBusiness(CourseService service) {
+        this.service = service;
+    }
+
+    public List<String> retrieveCoursesRelatedToSpring(String student) {
+        var filteredCourses = new ArrayList<String>();
+        var allCourses = service.retrieveCourses(student);
+
+        for(String course : allCourses) {
+            if (course.contains("Spring")) {
+                filteredCourses.add(course);
+            }
+        }
+
+        return filteredCourses;
+    }
+}
