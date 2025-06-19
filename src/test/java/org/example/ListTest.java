2526adfd15f9bc889dfda12a7a7e9b727ecfcc08
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-19 09:22:16 -0300

    mockando listas

---
+++ src/test/java/org/example/ListTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class ListTest {
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturn10() {
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10);
+
+        assertEquals(10, list.size());
+    }
+
+    @Test
+    void testMockingList_When_SizeIsCalled_ShouldReturnMultipleValues() {
+        List<?> list = mock(List.class);
+        when(list.size()).thenReturn(10).thenReturn(20);
+
+        assertEquals(10, list.size());
+        assertEquals(20, list.size());
+    }
+
+    @Test
+    void testMockingList_When_GetIsCalledWithArgumentsMatcher_ShouldReturn() {
+        var list = mock(List.class);
+        when(list.get(anyInt())).thenReturn("Erudio");
+
+        assertEquals("Erudio", list.get(anyInt()));
+        assertEquals("Erudio", list.get(anyInt()));
+    }
+}
