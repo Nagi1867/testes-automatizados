584f60f8685a361e8c52c1d53742d49aeb371fa7
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-06-23 20:31:54 -0300

    mockando data

--- src/test/java/org/example/service/OrderServiceTest.java
+++ src/test/java/org/example/service/OrderServiceTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import static org.mockito.Mockito.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceTest {
-    OrderService service = new OrderService();
-    Object defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final OrderService service = new OrderService();
+    private final Object defaultUuid = UUID.fromString("8d8b30e3-de52-4f1c-a71c-9905a8043dac");
+    private final LocalDateTime defaultLocalDateTime = LocalDateTime.of(2023, 7, 4, 15, 50);
 
     @Test
     void testShouldIncludeRandomOrderId_When_NoOrder() {
@@ -20,6 +22,17 @@ class OrderServiceTest {
             Order result = service.createOrder("MacBook", 2L, null);
 
             assertEquals(defaultUuid.toString(), result.getId());
+        }
+    }
+
+    @Test
+    void testShouldIncludeCurrentTime_When_CreateANewOrder() {
+        try(MockedStatic<LocalDateTime> mockedUuid = mockStatic(LocalDateTime.class)) {
+            mockedUuid.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
+
+            Order result = service.createOrder("MacBook", 2L, null);
+
+            assertEquals(defaultLocalDateTime, result.getCreationDate());
         }
     }
 }
